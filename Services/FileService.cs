dd4a2f6edc30c82f6277b005982dbc8ace694267
st10435769 <165911457+st10435769@users.noreply.github.com>
2024-08-13 20:57:45 +0200

    Add files via upload

---
+++ Services/FileService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
+using System.IO;
+using System.Threading.Tasks;
+
+public class FileService
+{
+    private readonly CloudFileClient _fileClient;
+
+    public FileService(string connectionString)
+    {
+        var storageAccount = CloudStorageAccount.Parse(connectionString);
+        _fileClient = storageAccount.CreateCloudFileClient();
+    }
+
+    public async Task UploadFileAsync(string shareName, string directoryName, string fileName, Stream fileStream)
+    {
+        var share = _fileClient.GetShareReference(shareName);
+        await share.CreateIfNotExistsAsync();
+
+        var rootDir = share.GetRootDirectoryReference();
+        var directory = rootDir.GetDirectoryReference(directoryName);
+        await directory.CreateIfNotExistsAsync();
+
+        var file = directory.GetFileReference(fileName);
+        await file.UploadFromStreamAsync(fileStream);
+    }
+
+    public async Task<CloudFile> GetFileAsync(string shareName, string directoryName, string fileName)
+    {
+        var share = _fileClient.GetShareReference(shareName);
+        var rootDir = share.GetRootDirectoryReference();
+        var directory = rootDir.GetDirectoryReference(directoryName);
+
+        var file = directory.GetFileReference(fileName);
+        return file;
+    }
+}
---
+++ Services/QueueService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System.Threading.Tasks;
+
+public class QueueService
+{
+    private readonly CloudQueueClient _queueClient;
+
+    public QueueService(string connectionString)
+    {
+        var storageAccount = CloudStorageAccount.Parse(connectionString);
+        _queueClient = storageAccount.CreateCloudQueueClient();
+    }
+
+    public async Task AddMessageAsync(string queueName, string message)
+    {
+        var queue = _queueClient.GetQueueReference(queueName);
+        await queue.CreateIfNotExistsAsync();
+        var cloudMessage = new CloudQueueMessage(message);
+        await queue.AddMessageAsync(cloudMessage);
+    }
+
+    public async Task<CloudQueueMessage> GetMessageAsync(string queueName)
+    {
+        var queue = _queueClient.GetQueueReference(queueName);
+        await queue.CreateIfNotExistsAsync();
+        var message = await queue.GetMessageAsync();
+        return message;
+    }
+}
